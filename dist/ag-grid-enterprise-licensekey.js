e494f888d016a57f914d7b0c90cf92fe4359dc9d
Paolo <paronne@servoy.com>
2018-09-05 12:33:48 +0300

    Update License Key

--- dist/ag-grid-enterprise-licensekey.js
+++ dist/ag-grid-enterprise-licensekey.js
@@ -1,7 +1,7 @@
 angular.module('aggridenterpriselicensekey', ['servoy']).factory('$aggridenterpriselicensekey',[function() {
 	return {
 		setLicenseKey: function() {
-			agGrid.LicenseManager.setLicenseKey("Servoy_Servoy_7Devs_1OEM_22_August_2018__MTUzNDg5MjQwMDAwMA==bf70d060fe7e90b9550a7821a54f6fa8");
+			agGrid.LicenseManager.setLicenseKey("Servoy_B.V._Servoy_7Devs_1OEM_22_August_2019__MTU2NjQyODQwMDAwMA==6c490d5c7f432e256493c8ca91624202");
 		}
 	}
 }]);
